df8af79a45632b0cdd9b1a557e45d4fee9d615ee
Zekai Kırıktaş <zkkiriktass@gmail.com>
2023-08-06 13:00:06 +0300

    ilk degisiklik

--- src/GitProjeKisisel/GitHubKisisel.java
+++ src/GitProjeKisisel/GitHubKisisel.java
@@ -3,5 +3,6 @@ package GitProjeKisisel;
 public class GitHubKisisel {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("That's it");
     }
 }
